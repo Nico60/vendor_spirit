5c538f1ffffa9def2d5c07cb2fb948020286999d
Nico60 <ndhouailly@gmail.com>
2014-08-01 20:55:49 +0200

    Bump to v1.1
    Change-Id: Ie82e101ef292ee2fab3bf07eded5ae91d1392884

--- config/common.mk
+++ config/common.mk
@@ -305,7 +305,7 @@ else
     endif
 endif
 
-SPIRIT_VERSION = v1.0
+SPIRIT_VERSION = v1.1
 SpiritRom_Version=4.4.4
 CM_MOD_VERSION := SpiritRom-$(SpiritRom_Version)-$(SPIRIT_VERSION)-$(shell date -u +%Y%m%d)$(CM_EXTRAVERSION)-$(CM_BUILD)
 
