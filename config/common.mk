SaberMod/android_vendor_omni

Saberize:  This includes all the current sabermod changes plus
some new defaults for gcc versions.

Change-Id: I29d53fdc190a368b7dc20f211cd3e0d8c1008c7f
Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

config/common.mk
@@ -70,3 +70,14 @@ endif
 
 # Add our overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/common
+
+# Inherit sabermod configs.  Default to arm if TARGET_ARCH is not defined.
+ifndef TARGET_ARCH
+  $(warning ********************************************************************************)
+  $(warning *  TARGET_ARCH not defined, defaulting to arm.)
+  $(warning *  To use arm64 set TARGET_ARCH := arm64)
+  $(warning *  in device tree before common.mk is called.)
+  $(warning ********************************************************************************)
+TARGET_ARCH := arm
+endif
+include vendor/omni/config/sm.mk

Nico60/vendor_spirit

Change-Id: I5a2547df89c8c0d6fd3c6e953011d0c7f854fcb2

config/common.mk
@@ -216,4 +216,15 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 
 -include vendor/cyngn/product.mk
 
+# Inherit sabermod configs.  Default to arm if TARGET_ARCH is not defined.
+ifndef TARGET_ARCH
+  $(warning ********************************************************************************)
+  $(warning *  TARGET_ARCH not defined, defaulting to arm.)
+  $(warning *  To use arm64 set TARGET_ARCH := arm64)
+  $(warning *  in device tree before common.mk is called.)
+  $(warning ********************************************************************************)
+TARGET_ARCH := arm
+endif
+include vendor/spirit/config/sm.mk
+
 $(call prepend-product-if-exists, vendor/extra/product.mk)